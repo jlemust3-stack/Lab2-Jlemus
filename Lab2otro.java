5cd5ba9f625552fa9865c22db7ce671061090ce8
jlemust3-stack <jlemust3@miumg.edu.gt>
2026-04-18 17:49:23 -0600

    ok
    se agrega lo que se trabaja en clase

---
+++ Lab2otro.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.lab2otro;
+
+/**
+ *
+ * @author umg
+ */
+public class Lab2otro {
+
+    public static void main(String[] args) {
+        login ig = new login();
+        ig.setVisible(true);
+    }
+}
